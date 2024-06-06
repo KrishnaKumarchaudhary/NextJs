fb0b7b830781210eab9c50050bcd311318324bf4
Krishna kumar chaudhary <ch.krishnakumar01@gmail.com>
2024-06-06 22:19:00 +0530

    Routing

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+export default function Home() {
+  return (
+    <main>
+      <Header />
+
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <a href="/about">About us</a>
+      </p>
+    </main>
+  );
+}
